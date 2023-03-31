f3218e503074005a2650f4460a7a23a25270347c
cyberfolk <andrea1995ferrari.com>
2023-03-31 10:53:35 +0200

    initial commit

---
+++ milestone-1/assets/js/Models/Task.js
@@ -0,0 +1,15 @@
+/**
+ * * PLACEHOLDER
+ */
+
+/* export */ class Task {
+  /**
+   * Image Contructor
+   * @param {String} text The text of the task src
+   * @param {boolean} done if true the task is completed
+   */
+  constructor(text, done) {
+    this.text = text
+    this.done = done
+  }
+}
---
+++ milestone-1/assets/js/app.js
@@ -0,0 +1,21 @@
+/**
+ * Milestone 1
+ * Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+ * Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto 
+ * */
+
+// import { tasks } from "./db.js"; // import of a default export
+// import { Task } from "./Models/Task.js"; // import of a default export
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {}
+    },
+    methods: {
+    },
+    mounted() {
+    },
+}
+).mount('#app')
