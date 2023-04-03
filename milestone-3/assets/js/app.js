c33104bae1c0d3c53654625105991d26bda24fa5
cyberfolk <andrea1995ferrari.com>
2023-04-03 10:37:05 +0200

    Add Class with DB but not work

---
+++ milestone-3/assets/js/app.js
@@ -0,0 +1,48 @@
+/**
+ * Milestone 1
+ * Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+ * Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto 
+ * */
+
+import { contacts } from "./db.js"; // import of a default export
+import { Contact } from "./Models/Contact.js"; // import of a default export
+import { Message } from "./Models/Message.js"; // import of a default export
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            activeContact: -1,
+            contacts: contacts,
+        }
+    },
+    methods: {
+        getLastMex(messages) {
+            const len = messages.length;
+            const message = messages[len - 1].message;
+            console.log(message.length);
+            if (message.length > 40) {
+                console.log(message.length);
+                return `${message.substring(0, 37)}...`;
+            } else {
+                return message;
+            }
+        },
+        getLastMexDate(messages) {
+            const len = messages.length;
+            //console.log(messages[len - 1].date);
+            const date = new Date(messages[len - 1].date);
+            //console.log(date);
+            //console.log(date.getMinutes());
+            return `${date.getHours()}:${date.getMinutes()}`
+        },
+        getActiveContact(index) {
+            this.activeContact = index;
+            console.log(this.activeContact);
+        }
+    },
+    mounted() {
+    },
+}
+).mount('#app')
